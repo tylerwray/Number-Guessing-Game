83a4e2a30cb21210750347aa9ca6d8c15e1804e2
Tyler Wray <wraytw@users.noreply.github.com>
2017-03-30 17:04:37 -0600

    Creating a base template for our program

--- game.asm
+++ game.asm
@@ -1,4 +1,22 @@
-; Beggining of the file for our number guessing game
-; Hello World!!!!!!!
-;annyeong!
-; Test edit
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;           Number Guessing Game           ;;;
+;;;           --------------------           ;;;
+;;;                                          ;;;
+;;;           Members:                       ;;;
+;;;             - Jonathan Trusdale          ;;;
+;;;             - Tyler Wray                 ;;;
+;;;             - Bekah Williams             ;;;
+;;;             - Justin Ward                ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+.ORIG x3000
+;
+HALT
+;;;;;;;;;;;;;;;;
+;   Constants  ;
+;;;;;;;;;;;;;;;;
+ANSWER .BLKW 1
+QUESTIONPRE .STRINGZ "Is "
+QUESTIONPOST .STRINGZ " your number?"
+VALIDATION .STRINGZ "Were we right? (0 = low, 1 = correct, 2 = high): "
+.END
